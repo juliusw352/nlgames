17e4171c186b6484a4dffde2d3486010afac5d8a
Julius W <julius.weisser@warwick.ac.uk>
2022-11-02 19:43:08 +0000

    experimenting with some stuff

--- src/nlgames/xorgames.py
+++ src/nlgames/xorgames.py
@@ -12,8 +12,24 @@ class xorgame:
 		if (np.sum(self.probMatrix) != 1):
 			raise ValueError("The probabilities must sum up to one.")
 
+
+	# TODO: Figure out expansion beyond simple binary games
 	def cvalue(self):
 		val = 0
 
-
 		return val
+
+q_0 = 2
+q_1 = 2
+
+for a_ans in range(2**q_0):
+	for b_ans in range(2**q_1):
+		print(a_ans)
+		print(b_ans)
+		#! in toqito: Index is question, value is answer!
+		a_vec = a_ans >> np.arange(q_0) & 1
+		a_vec_tmp = a_ans >> np.arange(q_0)
+		b_vec = b_ans >> np.arange(q_1) & 1
+
+		print(a_vec)
+		print(a_vec_tmp)
