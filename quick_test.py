100dd62153a296760fa5df3af6dd19d94f9ec5c6
Julius W <julius.weisser@warwick.ac.uk>
2023-05-01 13:25:14 +0100

    Don't worry about this commit

--- quick_test.py
+++ quick_test.py
@@ -1,6 +1,29 @@
 import numpy as np
+import time
+from nlgames.Xorgame import Xorgame
+from toqito.nonlocal_games.xor_game import XORGame
 
-arr = np.array([[2, 3], [-3, 9]])
+prob = np.array([[0.25,0.25],[0.25,0.25]])
+pred = np.array([[0,0],[0,1]])
 
-if np.any(arr<0):
-    print("a")
+game = Xorgame(pred, prob)
+game2 = XORGame(prob, pred, 1)
+
+nlg_sum = 0.0
+tq_sum = 0.0
+for i in range(10):
+    start = time.time()
+    print(game.nsval_single())
+    end = time.time()
+    nlg_time = end-start
+    nlg_sum = nlg_sum + nlg_time
+for i in range(10):
+    start = time.time()
+    print(game2.nonsignaling_value())
+    end = time.time()
+    tq_time = end-start
+    tq_sum = tq_sum + tq_time
+
+
+print("Toqito: " + str(tq_sum/10))
+print("Nlgames: " + str(nlg_sum/10))
